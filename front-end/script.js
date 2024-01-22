d1ce824c60faddc6e1ba43506eee0df614a23e4f
ashraf9932 <ashraf.neizk@gmail.com>
2024-01-22 13:15:19 +0200

    linking

--- front-end/script.js
+++ front-end/script.js
@@ -1,11 +1,23 @@
 var siteLang = localStorage.getItem('sitelang')
-console.log(siteLang);
 document.getElementById('lang-btn').onclick = function(){
     if(localStorage.getItem('sitelang') == 'en'){
         localStorage.setItem('sitelang','ar')
-        window.open('/front-end/ar/index.html','_self')
+        if(localStorage.getItem('page') == 'home'){
+            window.open('/front-end/ar/','_self')
+        }else{
+            window.open(`/front-end/ar/${localStorage.getItem('page')}/`,'_self')
+        }
     }else{
         localStorage.setItem('sitelang','en')
-        window.open('/front-end/en/index.html','_self')
+        if(localStorage.getItem('page') == 'home'){
+            window.open('/front-end/en/','_self')
+        }else{
+            window.open(`/front-end/en/${localStorage.getItem('page')}/`,'_self')
+        }
     }
 }
+document.querySelectorAll('.nav-list li').forEach((nav)=>{
+    nav.onclick = function(e) {
+        window.window.open(`/front-end/${localStorage.getItem('sitelang')}/${nav.dataset.page}`,'_self')
+    }
+})

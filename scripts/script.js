286f63b3479c6421b0d198175820ec60f9db1822
ashraf9932 <ashraf.neizk@gmail.com>
2024-02-08 14:19:13 +0200

    fix nav redirect

--- scripts/script.js
+++ scripts/script.js
@@ -3,22 +3,22 @@ document.getElementById('lang-btn').onclick = ()=>{
     if(localStorage.getItem('sitelang') == 'en'){
         localStorage.setItem('sitelang','ar')
         if(localStorage.getItem('page') == 'home'){
-            window.open('/front-end/ar/','_self')
+            window.open('/ar/','_self')
         }else{
-            window.open(`/front-end/ar/${localStorage.getItem('page')}/`,'_self')
+            window.open(`/ar/${localStorage.getItem('page')}/`,'_self')
         }
     }else{
         localStorage.setItem('sitelang','en')
         if(localStorage.getItem('page') == 'home'){
-            window.open('/front-end/en/','_self')
+            window.open('/en/','_self')
         }else{
-            window.open(`/front-end/en/${localStorage.getItem('page')}/`,'_self')
+            window.open(`/en/${localStorage.getItem('page')}/`,'_self')
         }
     }
 }
 document.querySelectorAll('.nav-list li').forEach((nav)=>{
     nav.onclick = function(e) {
-        window.window.open(`/front-end/${localStorage.getItem('sitelang')}/${nav.dataset.page}`,'_self')
+        window.window.open(`/${localStorage.getItem('sitelang')}/${nav.dataset.page}`,'_self')
     }
 })
 var pageHeader = document.getElementById('navHeader')

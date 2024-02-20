9187b231fe6e42df19e196529a7cd4ef1ea4a4f3
ashraf9932 <ashraf.neizk@gmail.com>
2024-02-20 04:29:58 +0200

    contact responsive Done

--- scripts/contact.js
+++ scripts/contact.js
@@ -10,7 +10,6 @@ document.querySelector('.contact-arrow-btn').onclick = ()=>{
             e.classList.add('switch')
         })
     }
-    document.querySelector('.contact-arrow-btn').classList.toggle('switch')
 }
 window.addEventListener('wheel',(e)=>{
     if(e.deltaY > 0 ){
@@ -18,12 +17,6 @@ window.addEventListener('wheel',(e)=>{
         document.querySelector('.contact-arrow-btn').classList.add('switch')
         document.querySelectorAll('.contact-card,.visit-card,.form-card').forEach((e)=>{
             e.classList.add('switch')
-        })
-    }else{
-        window.scroll({top:0,left:0,behavior:'smooth'})
-        document.querySelector('.contact-arrow-btn').classList.remove('switch')
-        document.querySelectorAll('.contact-card,.visit-card,.form-card').forEach((e)=>{
-            e.classList.remove('switch')
         })
     }
 })
